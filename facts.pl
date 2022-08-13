497ba1b131df7b3950403eb54603819e42c41b09
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2022-08-13 17:05:55 -0400

    Our implementation

---
+++ facts.pl
@@ -0,0 +1,16 @@
+dateofbirth(napoleonbonaparte,"15august1769").
+partof(revolutionarywars,"frenchrevolution").
+haspart(frenchrevolution,"revolutionarywars").
+partof(frenchrevolution,"revolutionarywars").
+haspart(revolutionarywars,"frenchrevolution").
+country(firstconsul,"frenchrepublic").
+headofstate(frenchrepublic,"napoleon").
+legislativebody(frenchrepublic,"firstconsul").
+positionheld(napoleonbonaparte,"emperorofthefrench").
+officeholder(emperorofthefrench,"napoleonbonaparte").
+pointintime(1815,"1815").
+occupation(napoleon,"political").
+conflict(napoleon,"political").
+dateofdeath(napoleonbonaparte,"15may1821").
+dateofdeath(bonaparte,"15may1821").
+pointintime(diedon15may1821,"15may1821").
